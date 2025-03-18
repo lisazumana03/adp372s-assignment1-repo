c9427cb72beffa0b212f2390e70804c3b078e2cc
lisazumana03 <zumanalisa@gmail.com>
2025-03-18 17:26:35 +0200

    loading files

--- src/main/java/za/ac/cput/domain/Booking.java
+++ src/main/java/za/ac/cput/domain/Booking.java
@@ -1,13 +1,79 @@
 package za.ac.cput.domain;
 
+import java.util.Date;
+import java.util.Locale;
+
 public class Booking {
-    protected int booking_id;
+    protected int bookingID;
+    protected int userID;
+    protected int carID;
+    protected Date startDate;
+    protected Date endDate;
+    protected String status;
 
-    private Booking(int booking_id) {
-        this.booking_id = booking_id;
-    }
-    public int getBooking_id() {
-        return booking_id;
+    private Booking(Builder builder){
+        this.bookingID = builder.bookingID;
+        this.userID = builder.userID;
+        this.carID = builder.carID;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+        this.status = builder.status;
     }
 
+    public int getBookingID() {
+        return bookingID;
+    }
+    public int getUserID() {
+        return userID;
+    }
+    public int getCarID() {
+        return carID;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public static class Builder {
+        private int bookingID;
+        private int userID;
+        private int carID;
+        private Date startDate;
+        private Date endDate;
+        private String status;
+
+        public Builder setBookingID(int bookingID) {
+            this.bookingID = bookingID;
+            return this;
+        }
+        public Builder setUserID(int userID) {
+            this.userID = userID;
+            return this;
+        }
+        public Builder setCarID(int carID) {
+            this.carID = carID;
+            return this;
+        }
+        public Builder setStartDate(Date startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+        public Builder setEndDate(Date endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+        public Builder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+        public Booking build(){
+            return new Booking(this);
+        }
+
+    }
 }
