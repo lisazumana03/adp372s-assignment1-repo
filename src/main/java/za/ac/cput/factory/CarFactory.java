7f7e584e1861906708e1029ffa7f8b2b53969f41
ImtiyaazWaggie <waggieimtiyaaz@gmail.com>
2025-03-24 10:25:50 +0200

    created the Car Factory Class for Car hiring system

---
+++ src/main/java/za/ac/cput/factory/CarFactory.java
@@ -0,0 +1,32 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Car;
+
+public class CarFactory {
+    public static Car createCar(int carID, String model, String brand, int year, boolean availability, double rentalPrice) {
+        return new Car.Builder()
+                .setCarID(carID)
+                .setModel(model)
+                .setBrand(brand)
+                .setYear(year)
+                .setAvailability(availability)
+                .setRentalPrice(rentalPrice)
+                .build();
+    }
+
+    public static Car updateAvailability(Car car, boolean availability) {
+        return new Car.Builder()
+                .setCarID(car.getCarID())
+                .setModel(car.getModel())
+                .setBrand(car.getBrand())
+                .setYear(car.getYear())
+                .setAvailability(availability)
+                .setRentalPrice(car.getRentalPrice())
+                .build();
+    }
+
+    public static double calculateRentalPrice(Car car, int rentalDays) {
+        return car.getRentalPrice() * rentalDays;
+    }
+}
+
