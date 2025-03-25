cda795fdfdee428671e6c25e2b18c7324099316d
ImtiyaazWaggie <waggieimtiyaaz@gmail.com>
2025-03-25 11:46:36 +0200

    a new Car Factory Class for Car hiring system

--- src/main/java/za/ac/cput/factory/CarFactory.java
+++ src/main/java/za/ac/cput/factory/CarFactory.java
@@ -1,10 +1,30 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Car;
-// Imtiyaaz Waggie 219374759 24/03/2025//
+
+/*
+ * Imtiyaaz Waggie 219374759s
+ * Date: 25/03/2025
+ */
 
 public class CarFactory {
     public static Car createCar(int carID, String model, String brand, int year, boolean availability, double rentalPrice) {
+        if (carID < 0 || carID > 100000) {
+            return null;
+        }
+        if (model == null || model.isEmpty()) {
+            return null;
+        }
+        if (brand == null || brand.isEmpty()) {
+            return null;
+        }
+        if (year < 1886 || year > 2100) {
+            return null;
+        }
+        if (rentalPrice < 0) {
+            return null;
+        }
+
         return new Car.Builder()
                 .setCarID(carID)
                 .setModel(model)
@@ -16,6 +36,9 @@ public class CarFactory {
     }
 
     public static Car updateAvailability(Car car, boolean availability) {
+        if (car == null) {
+            return null;
+        }
         return new Car.Builder()
                 .setCarID(car.getCarID())
                 .setModel(car.getModel())
@@ -25,9 +48,4 @@ public class CarFactory {
                 .setRentalPrice(car.getRentalPrice())
                 .build();
     }
-
-    public static double calculateRentalPrice(Car car, int rentalDays) {
-        return car.getRentalPrice() * rentalDays;
-    }
 }
-
