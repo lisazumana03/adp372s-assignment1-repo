fae19eeb8ae7622747cc526b45beb845355e0ae0
lisazumana03 <zumanalisa@gmail.com>
2025-03-26 21:51:54 +0200

    loading files

--- src/test/java/za/ac/cput/factory/BookingFactoryTest.java
+++ src/test/java/za/ac/cput/factory/BookingFactoryTest.java
@@ -3,13 +3,19 @@ package za.ac.cput.factory;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Booking;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingFactoryTest {
 
     @Test
     void createBooking() {
-        Booking booking = BookingFactory.createBooking(34,030, 245, );
+
+        Date startDate = new Date(2025-3-12);
+        Date endDate = new Date(2028-3-13);
+
+        Booking booking = BookingFactory.createBooking(34,030, 245, startDate, endDate, "Booking approved! Congratulations!");
     }
 
     @Test
