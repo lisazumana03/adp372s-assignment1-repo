b13e9b9f5282a82d535ecb3b146c0cdfe518527f
lisazumana03 <zumanalisa@gmail.com>
2025-03-26 21:51:37 +0200

    loading files

---
+++ src/test/java/za/ac/cput/repository/BookingRepositoryImplTest.java
@@ -0,0 +1,43 @@
+package za.ac.cput.repository;
+
+/*
+Author: Lisakhanya Zumana (230864821)
+Date 26/03/2025
+ */
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class BookingRepositoryImplTest {
+
+    private static BookingRepository bookingRepository = new BookingRepositoryImpl();
+    private static Bookin;
+
+    @Test
+    void getRepository() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    @Order(1)
+    void create() {
+
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
