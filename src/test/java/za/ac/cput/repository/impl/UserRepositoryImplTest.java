c7aacf9f4bca66ab8e435be2b36dc67ee5adb608
BongaVelem <bongavelem@outlook.com>
2025-03-27 22:54:47 +0200

    Added Test files and Repo, please run on your systems to see if it works .

---
+++ src/test/java/za/ac/cput/repository/impl/UserRepositoryImplTest.java
@@ -0,0 +1,78 @@
+package za.ac.cput.repository.impl;
+// User UserRepositoryImplTest  class
+//Author: Bonga Velem (220052379)
+//Date: 27 March 2025
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.User;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryImplTest {
+    private static UserRepositoryImpl repository ;
+    private static User user;
+
+
+    @BeforeEach
+    void setUp() {
+        repository = (UserRepositoryImpl) UserRepositoryImpl.getRepository();
+        user = new User.Builder()
+                .setUserId(1)
+                .setName("Bonga Velem")
+                .setEmail("bongavelem@example.com")
+                .setPhoneNumber("0798135811")
+                .setLicenseNumber("ABC12345")
+                .build();
+    }
+
+
+    @Test
+    void create() {
+        User createdUser = repository.create(user);
+        assertNotNull(createdUser);
+        assertEquals(user.getUserId(), createdUser.getUserId());
+        assertEquals(user.getEmail(), createdUser.getEmail());
+
+    }
+
+    @Test
+    void read() {
+        repository.create(user);
+        User retrievedUser = repository.read(user.getUserId());
+        assertNotNull(retrievedUser);
+        assertEquals(user.getUserId(), retrievedUser.getUserId());
+        assertEquals(user.getName(), retrievedUser.getName());
+    }
+
+    @Test
+    void update() {
+        repository.create(user);
+
+
+        User updatedUser = new User.Builder()
+                .copy(user)
+                .setName("Bonga Velem")
+                .setEmail("bongav22@gmail.com")
+                .build();
+
+        User result = repository.update(updatedUser);
+        assertNotNull(result);
+        assertEquals("Bonga Velem", result.getName());
+        assertEquals("bongav22@gmail.com", result.getEmail());
+    }
+
+
+
+    @Test
+    void delete() {
+        repository.create(user);
+        repository.delete(user);
+        User deletedUser = repository.read(user.getUserId());
+        assertNull(deletedUser);
+    }
+
+
+}
