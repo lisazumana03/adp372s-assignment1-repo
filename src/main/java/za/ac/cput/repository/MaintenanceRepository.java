fbd890ef1075beabed8f56d7af4238d86b8cd62f
NohambaS <164313427+NohambaS@users.noreply.github.com>
2025-03-27 14:48:14 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/repository/MaintenanceRepository.java
@@ -0,0 +1,9 @@
+package za.ac.cput.repository;
+/*
+ * Sibulele Gift Nohamba (220374686)
+ * Date: 25/03/2025
+ * */
+import za.ac.cput.domain.Maintenance;
+
+public interface MaintenanceRepository extends IRepository<Maintenance, Integer> {
+}
