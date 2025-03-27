c909025a91ba7282fed4a8870926041e0f8e4280
ImtiyaazWaggie <waggieimtiyaaz@gmail.com>
2025-03-27 11:27:24 +0200

    Test CarFactory and CarRepository

---
+++ src/test/java/za/ac/cput/factory/CarFactoryTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//Imtiyaaz Waggie 219374759 27/03/2025//
+
+class CarFactoryTest {
+
+    @Test
+    void createCar() {
+    }
+
+    @Test
+    void updateAvailability() {
+    }
+}
---
+++ src/test/java/za/ac/cput/repository/CarRepositoryImplTest.java
@@ -0,0 +1,33 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//Imtiyaaz waggie 219374759 27/03/2025//
+
+class CarRepositoryImplTest {
+
+    @Test
+    void getRepository() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void updateAvailability() {
+    }
+}
